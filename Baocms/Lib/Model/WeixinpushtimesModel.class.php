42b1549217165eb8c7cfdbd6be3010c2df73259b
WIN7U-20160622J\Administrator <sai530530@gmail.com>
2018-04-16 23:01:17 +1200

    添加weixin时段广告admin添加编辑等功能；添加推送记录model；添加推送次数model

---
+++ Baocms/Lib/Model/WeixinpushtimesModel.class.php
@@ -0,0 +1,10 @@
+<?php
+
+
+
+class WeixinpushtimesModel extends CommonModel{
+    protected $pk   = 'times_id';
+    protected $tableName =  'weixin_push_times';
+    protected $token = 'weixin_push_times';
+
+}
